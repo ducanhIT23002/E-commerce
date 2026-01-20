82dd0154d4a9127e76d96b5d53f0ef7246c0be22
DucAnh <ducanh200574@gmail.com>
2026-01-20 14:29:43 +0700

    chore: update API URL to production backend

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8080/api', 
+  baseURL: 'https://shop-backend-6lun.onrender.com/api', 
   headers: {
     'Content-Type': 'application/json',
   },
